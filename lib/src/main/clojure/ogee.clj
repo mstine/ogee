a67e6c21576224479a42028946b557fea8e2c60c
Roman Roelofsen <roman.roelofsen@gmail.com>
2009-10-03 23:50:19 +0200

    added automatic restart

--- lib/src/main/clojure/ogee.clj
+++ lib/src/main/clojure/ogee.clj
@@ -3,14 +3,21 @@
      ogee.osgi
      [clojure.contrib.logging :as logging]
      [clojure.contrib.server-socket :as server-socket]))
-					 
+
+(def telnet-server (ref nil))
+
 (defn ogee-start [context]
   (dosync (ref-set ogee.osgi/bundle-context context))
   (logging/info "Starting Telnet server on port 9999")
-  (server-socket/create-repl-server 9999)
+  (dosync (ref-set telnet-server (server-socket/create-repl-server 9999)))
   (logging/info "Ogee started."))
 
 (defn ogee-stop []
-	"Shutdown the Ogee runtime."
-	(logging/info "Stopping Ogee..."))
+  "Shutdown the Ogee runtime."
+  (logging/info "Stopping Ogee...")
+  (server-socket/close-server @telnet-server))
+
+
+
+
 
--- runtime/src/main/java/org/ogee/Activator.java
+++ runtime/src/main/java/org/ogee/Activator.java
@@ -5,27 +5,42 @@ import java.io.FilenameFilter;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.service.packageadmin.PackageAdmin;
+import org.osgi.util.tracker.ServiceTracker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Activator implements BundleActivator {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	private static final String CLJS_DIR = "cljs.dir";
 	private ClojureRuntime clojureRuntime;
+	private String cljsDir;
+	private boolean shutdown = false;
+	private BundleContext context;
 
 	public void start(BundleContext context) throws Exception {
-		String cljsDir = context.getProperty(CLJS_DIR);
+		this.context = context;
+		
+		cljsDir = context.getProperty(CLJS_DIR);
 		cljsDir = cljsDir == null ? "cljs" : cljsDir;
 
-		clojureRuntime = new ClojureRuntime(context, getAllCljs(cljsDir));
+		clojureRuntime = new ClojureRuntime(context, getAllCljs());
 		clojureRuntime.init();
+
+		startDirWatcher();
 	}
 
 	public void stop(BundleContext context) throws Exception {
+		this.shutdown  = true;
 		clojureRuntime.destroy();
 	}
 
-	private URL[] getAllCljs(String cljsDir) {
+	private URL[] getAllCljs() {
 		File[] allFiles = new File(cljsDir).listFiles(new FilenameFilter() {
 			public boolean accept(File dir, String name) {
 				if (name.endsWith(".jar"))
@@ -45,4 +60,34 @@ public class Activator implements BundleActivator {
 		return fileUrls;
 	}
 
+	private void startDirWatcher() {
+		final long started = System.currentTimeMillis();
+		new Thread() {
+			public void run() {
+				try {
+				while (!Activator.this.shutdown) {
+						Thread.sleep(500);
+					URL[] allCljs = getAllCljs();
+					for (URL url : allCljs) {
+						File f = new File(url.toURI());
+						if (f.lastModified() > started) {
+							Activator.this.shutdown = true;
+							restartOgee();
+						}
+					}
+				}
+				} catch (Exception e) {
+					e.printStackTrace();
+					return;
+				}
+			}
+		}.start();
+	}
+	
+	private void restartOgee() {
+		logger.info("Restarting Ogee...");
+		ServiceTracker st = new ServiceTracker(context, PackageAdmin.class.getName(), null);
+		st.open();
+		((PackageAdmin) st.getService()).refreshPackages(new Bundle[] {context.getBundle()});
+	}
 }
