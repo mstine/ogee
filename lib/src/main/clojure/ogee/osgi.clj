e90016dd24978b1059681bada01a2d48ea2e6aa1
Roman Roelofsen <roman.roelofsen@gmail.com>
2009-10-19 19:55:13 +0200

    removed service map concept

--- lib/src/main/clojure/ogee/osgi.clj
+++ lib/src/main/clojure/ogee/osgi.clj
@@ -22,21 +22,13 @@
     (.open tracker)
     tracker))
 
-(defn smap-import
-  "Returns a proxy to the service map with a matching name."
-  [sname]
-  (let [ldap (str "(ogee.service.name=" (str sname) ")")
-        tracker (service-tracker java.util.Map ldap)]
-    (ogee.utils/aop-proxy java.util.Map (fn [obj mth args] (.invoke mth (.getService tracker) args)))))
-
-(defn smap-export
-  "Exports service map 'service' with name 'sname'."
-  [sname service]
-  (.registerService @bundle-context "java.util.Map" service (ogee.utils/map-to-hashtable {"ogee.service.name" (str sname)})))
-
 (defn register-servlet
   [servlet url]
-  (.registerService @bundle-context "javax.servlet.Servlet" servlet (ogee.utils/map-to-hashtable {"alias" (str url)})))
+  (.registerService 
+    @bundle-context
+    (.getName javax.servlet.Servlet)
+    servlet
+    (ogee.utils/map-to-hashtable {"alias" (str url)})))
 
 (defn- register-managed-service
   [pid]
