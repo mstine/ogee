5928eb437447a7875d5f2fd9e9d2cbdf0c19493e
Roman Roelofsen <roman.roelofsen@gmail.com>
2009-10-21 10:29:28 +0200

    simplified cop macros

--- library/src/main/clojure/ogee/cop.clj
+++ library/src/main/clojure/ogee/cop.clj
@@ -5,36 +5,20 @@
 
 (defmacro deflayer
   [layer-name variations]
-  (let [layer-name-str# (str layer-name)
-        grouped# (mapcat
+  (let [layer-name-str (str layer-name)
+        grouped (mapcat
                   (fn [p] [`(var ~(first p)) (second p)])
                   (partition 2 variations))]
-    `(def ~layer-name {:name ~layer-name-str#
-                       :variations (hash-map ~@grouped#)})))
+    `(def ~layer-name {:name ~layer-name-str
+                       :variations (hash-map ~@grouped)})))
 
 (defmacro with-layers
   [layers & body]
-  (let [layers# (eval layers)
-        merged-name# (str-utils/str-join "->" (map :name layers#))
-        merged-variations# (reduce merge (map :variations layers#))
-        ]
-    `(let []
-       (push-thread-bindings ~merged-variations#)
-       (try
-         ~@body
-         (finally
-           (pop-thread-bindings))))))
+  `(let [merged-name# (str-utils/str-join "->" (map :name ~layers))
+         merged-variations# (reduce merge (map :variations ~layers))]
+     (push-thread-bindings merged-variations#)
+     (try
+       ~@body
+       (finally
+         (pop-thread-bindings)))))
 
-
-
-; TESTS
-(def *a*)
-(def *b*)
-(def *c*)
-(deflayer l1 [*a* 111])
-(deflayer l2 [*b* 222])
-(deflayer l3 [*c* 333])
-(println "------------------------------")
-(with-layers [l1 l2 l3]
-  (println *a*)
-  )
