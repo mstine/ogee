68f4c8443d9efae7f744482882f89041341d48e9
Roman Roelofsen <roman.roelofsen@gmail.com>
2009-10-21 17:06:59 +0200

    impart working

--- library/src/main/clojure/ogee/cop.clj
+++ library/src/main/clojure/ogee/cop.clj
@@ -1,7 +1,10 @@
 
 (ns ogee.cop
   (:require
-    [clojure.contrib.str-utils :as str-utils]))
+    [clojure.contrib.str-utils :as str-utils]
+    [clojure.contrib.logging :as logging]))
+
+(def *active-layers*)
 
 (defmacro deflayer
   [layer-name variations]
@@ -17,8 +20,30 @@
   `(let [merged-name# (str-utils/str-join "->" (map :name ~layers))
          merged-variations# (reduce merge (map :variations ~layers))]
      (push-thread-bindings merged-variations#)
-     (try
-       ~@body
-       (finally
-         (pop-thread-bindings)))))
+     (binding [*active-layers* ~layers] ; TODO merge with already active layers
+       (try
+         ~@body
+         (catch Throwable t#
+           (logging/error (str "Exception thrown. Active layers: " merged-name#) t#)
+           (throw t#))
+         (finally
+           (pop-thread-bindings))))))
+
+(defmacro impart
+  [f & initargs]
+  `(let [current-layers# *active-layers*]
+     (fn [& args#]
+       (with-layers current-layers#
+         (apply ~f (concat ~(vec initargs) args#)))))
+  )
+
+(def *a*)
+(deflayer l1 [*a* 1])
+(defn pi [a b] (println a "and" b ":" *a*))
+(with-layers [l1]
+  (doall (map (partial pi "a") ["b"]))
+  (doall (pmap (impart pi "x") ["y"]))
+  ;(impart pi)
+  )
+
 
--- library/src/test/clojure/ogee/test_cop.clj
+++ library/src/test/clojure/ogee/test_cop.clj
@@ -3,27 +3,47 @@
   (:require 
     [ogee.cop :as cop])
   (:use
-    clojure.test
-    clojure.contrib.def))
+    clojure.test))
 
-(defvar *var1* "Var used in tests")
-(defvar *var2* "Var used in tests")
-(cop/deflayer layer1 [*var1* 1
-                          *var2* 2])
+(def *var1*) ; var used in layer 1
+(def *var2*) ; var used in layer 1
+(cop/deflayer layer1 [*var1* 1 *var2* 2])
+(cop/deflayer layer1-override [*var1* 11])
+
+(def *var3*) ; var used in layer 2
+(cop/deflayer layer2 [*var3* 3])
 
 (deftest create-layer
-  (is (var? #'layer1))
   (is (map? layer1))
   (is (= (:name layer1) "layer1"))
   (is (map? (:variations layer1)))
   (is (= ((:variations layer1) #'*var1*) 1))
   (is (= ((:variations layer1) #'*var2*) 2)))
 
+(deftest with-single-layer
+  (cop/with-layers [layer1]
+    (is (= *var1* 1))
+    (is (= *var2* 2))))
 
+(deftest with-multiple-layers
+  (cop/with-layers [layer1 layer2]
+    (is (= *var1* 1))
+    (is (= *var2* 2))
+    (is (= *var3* 3))))
 
+(deftest with-multiple-layers-validate-overrides
+  (cop/with-layers [layer1 layer1-override]
+    (is (= *var1* 11))
+    (is (= *var2* 2))))
 
-;(cop/with-layers [layer1]
-;  (println "XXX"))
+(deftest method-bind
+  (cop/with-layers [layer1]
+    (let [test-fn #(first (map (fn [_] *var1*) [1]))]
+      (println (test-fn))
+      ;(is (= @(test-fn) 1))
+
+      )))
+
 
 
 
