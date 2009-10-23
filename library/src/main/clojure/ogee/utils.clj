8169f77ae6e585ad67012121a2e41dfe5c4dc2b3
Roman Roelofsen <roman.roelofsen@gmail.com>
2009-10-23 10:58:04 +0200

    added util function

--- library/src/main/clojure/ogee/utils.clj
+++ library/src/main/clojure/ogee/utils.clj
@@ -1,2 +1,9 @@
 (ns ogee.utils
- (:require [clojure.contrib.logging :as logging]))
+ (:require 
+   [clojure.contrib.str-utils :as str-utils]
+   [clojure.contrib.logging :as logging]))
+
+(defn exception-add-text
+  "Wrap exception e in a RuntimeException and add texts separated by ' ' as message."
+  [e & texts]
+  (RuntimeException. (str-utils/str-join " " texts) e))
